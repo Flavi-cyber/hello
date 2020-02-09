e969215adfd84c4a4defe00c9b359eb69c98c5e6
LaichR <46196385+LaichR@users.noreply.github.com>
2020-02-09 14:56:06 +0100

    add function to parse a decimal integer number

--- Parse/ParseNumber.java
+++ Parse/ParseNumber.java
@@ -14,8 +14,11 @@ public class ParseNumber
         int val = 0;
         try{
           val = Integer.parseInt( args[0] );
-          sb.append( "; parsed value = ");
+          sb.append( "\nparsed value = ");
           sb.append( val );
+          
+          sb.append( "\nmy own parse function: ");
+          sb.append( ParseInt(args[0].toCharArray() ));
         }
         catch(NumberFormatException e)
         {
@@ -25,4 +28,19 @@ public class ParseNumber
       }
       System.out.println(sb.toString());
    }
+   
+   static int ParseInt( char[] characters) throws NumberFormatException
+   {
+      int value = 0;
+      for( int i = 0; i < characters.length; i++ )
+      {
+         if( !Character.isDigit(characters[i]) )
+         {
+            throw new NumberFormatException();
+         }
+         value *=10;
+         value += characters[i] - '0';
+      }
+      return value;
+   }   
 }
